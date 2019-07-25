904da29f07f5946b9c5711f06491e0b3215d55e0
fdiarte1 <francisco.diarte@kubra.com>
2019-07-25 16:10:55 -0700

    added additional info key value pair to request

--- src/main/java/com/kubra/apns/model/inbound/APNSPayload.java
+++ src/main/java/com/kubra/apns/model/inbound/APNSPayload.java
@@ -2,13 +2,15 @@ package com.kubra.apns.model.inbound;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
-import com.kubra.apns.model.inbound.APNSNotification;
 
 public class APNSPayload {
 
     @JsonProperty("aps")
     @SerializedName("aps")
     private APNSNotification notification;
+
+    @JsonProperty("additionalInfo")
+    private Object additionalInfo;
 
     public APNSPayload() { }
 
@@ -18,5 +20,13 @@ public class APNSPayload {
 
     public void setNotification(APNSNotification notification) {
         this.notification = notification;
+    }
+
+    public Object getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(Object additionalInfo) {
+        this.additionalInfo = additionalInfo;
     }
 }
